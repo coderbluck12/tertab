d844cbc8c19b16d515f082772ed9e7c59b7649ff
Oyenola Philip <76276196+oyenolaphilipinc@users.noreply.github.com>
2025-07-31 19:55:52 +0100

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,5 +24,18 @@ export default {
         'bg-yellow-100',
         'text-yellow-900',
         'text-yellow-800',
+        // Stat card colors - prevent purging of dynamic classes
+        'border-blue-500',
+        'text-blue-600',
+        'text-blue-500',
+        'border-green-500',
+        'text-green-600',
+        'text-green-500',
+        'border-teal-500',
+        'text-teal-600',
+        'text-teal-500',
+        'border-black-500',
+        'text-black-600',
+        'text-black-500',
     ],
 };
