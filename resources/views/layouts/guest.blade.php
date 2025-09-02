ba344179aec6c87a4e2e1011661a9e944e5496ab
Oyenola Philip <oyenolaphilip89@gmail.com>
2025-09-02 09:44:38 +0100

    done

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -65,6 +65,17 @@
             {{ $slot }}
         </div>
     </div>
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/68b5cd2b862a55192487ab18/1j432iu39';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
     </body>
     <script>
         document.getElementById('menu-toggle').addEventListener('click', function() {
